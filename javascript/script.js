e3d57ca159fd8d85c05ab26d88ee9b2bd9533fcd
Ju <hyojukim@Hyos-iMac.local>
2017-03-02 12:25:19 -0800

    Page 4 and broken functions
    Page 4 is newly added and fixed broken functions (e.g. toggle page
    nav).

--- javascript/script.js
+++ javascript/script.js
@@ -38,3 +38,88 @@ $(function(){
 			    });
 			})(jQuery);
 			
+    $(document).ready(function(){
+        //main-character
+        $(".coco-layon").hide();
+        $(".coco-layback").click(function(){
+            $(".coco-layback").hide();
+            $(".coco-layon").show();
+        $(".coco-layon").click(function(){
+            $(".coco-layon").hide();
+            $(".coco-layback").show();
+        });
+        });
+        //text-box
+        $(".text-box").hide();
+        $(".text-btn").click(function(){
+            $(".text-box").toggle();
+        });
+       
+        $(".textbox2").hide();
+        $(".text-btn2").click(function(){
+            $(".textbox2").toggle();
+        });
+
+        $(".textbox3").hide();
+        $(".text-btn3").click(function(){
+            $(".textbox3").toggle();
+        });
+
+        $(".pagenav").hide();
+        $(".pagenav-icon-container").click(function(){
+            $(".pagenav").toggle();
+        });
+
+        $(".doorOpen").hide();
+        $(".door").click(function(){
+            $(".doorOpen").show();
+            $(".doorOpen").click(function(){
+            	$(".doorOpen").hide();
+            });
+
+        });
+
+        $(".nkflag").click(function(){
+        	$(".nkflag").animate({transform: "scale(2)"}, 5000, 'linear');
+        });
+
+        $(".nkflag").click(function(){
+        	$(".info1").css('visibility','visible').fadeIn(400);
+        	$(".info1").click(function(){
+        		$(".info1").hide();
+        	});
+        });
+        $(".redtag").click(function(){
+            $(".container-open").toggleClass("main-nav-container");
+        });
+    
+    });
+
+
+    $(document).ready(function(){
+    	$(".notification").css('visibility', 'visible').animate({opacity:1.0}, 2000).delay(3000).fadeOut(800);
+    });
+
+    function allowDrop(ev) {
+    ev.preventDefault();
+    }
+
+    function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    }   
+
+    function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+// (function(){
+//     var first=true;
+//     $('.notification').click(function(){
+//         if (first){
+//             first=false;
+// 			$(".notification").css('visibility', 'visible').animate({opacity:1.0}, 2000).delay(3000).fadeOut(800);
+//         }
+//     });
+// })();
